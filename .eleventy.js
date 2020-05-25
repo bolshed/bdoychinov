202adce5d34260d6f09e57dfca94af3902862be9
bolshed <bolshed@protonmail.com>
2020-05-25 15:24:48 +0200

    seo3

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
-const pluginSEO = require("eleventy-plugin-seo");
-
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('assets');
-    eleventyConfig.addPlugin(pluginSEO, require("./src/_data/seo.json"));
 };
 
 return {
