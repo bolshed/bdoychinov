8f8aa13e7cc38d363a7eb1e5d9541a50363dfd32
bolshed <bolshed@protonmail.com>
2020-05-26 13:56:45 +0200

    seo4

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+const pluginSEO = require("eleventy-plugin-seo");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.addPlugin(pluginSEO, {
+        title: "Boyan Doychinov - A web developer from Berlin",
+        description: "An aspriring web developer looking to land an entry-level job in the tech-field.",
+        author: "Boyan Doychinov",
+      });
 };
 
 return {
